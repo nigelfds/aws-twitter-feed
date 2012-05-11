d44e0810d28dc0a9d8ca4c4ce3f5800989b3af19
Clarence Bakirtzidis <clarenceb@gmail.com>
2012-05-11 17:32:49 +1000

    Updated Twitter feed to use Twitter Bootstrap
    - Displays hash tag used for search at the top

--- lib/twitter_feed.rb
+++ lib/twitter_feed.rb
@@ -6,6 +6,6 @@ class TwitterFeed < Sinatra::Application
   get '/' do
     hash_tag = params[:hash_tag] || 'aws'
     statuses = Twitter.search "##{hash_tag}"
-    erb :index, :locals => {:statuses => statuses}
+    erb :index, :locals => {:statuses => statuses, :hash_tag => hash_tag }
   end
 end
